bc6145028eaa31da43f252361a37a27b6ca65ed9
Peterson Fonseca <petersonjf@gmail.com>
2018-11-24 12:14:11 -0200

    Initial commit

---
+++ app/src/main/java/br/com/planosemeador/planosemeador/Usuario.java
@@ -0,0 +1,32 @@
+package br.com.planosemeador.planosemeador;
+
+public class Usuario {
+
+    String nomeUsuario;
+    String telefoneUsuario;
+
+
+
+    public Usuario() {
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getTelefoneUsuario() {
+        return telefoneUsuario;
+    }
+
+    public void setTelefoneUsuario(String telefoneUsuario) {
+        this.telefoneUsuario = telefoneUsuario;
+    }
+
+
+
+
+}
