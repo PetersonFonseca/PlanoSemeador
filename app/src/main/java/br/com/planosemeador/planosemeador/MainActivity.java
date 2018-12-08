a6737f9a57fb8d86aaedee75e3fc6532947942ba
Peterson Fonseca <petersonjf@gmail.com>
2018-12-08 16:11:57 -0200

    ajustando icones menu

--- app/src/main/java/br/com/planosemeador/planosemeador/MainActivity.java
+++ app/src/main/java/br/com/planosemeador/planosemeador/MainActivity.java
@@ -87,6 +87,7 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
         navigationView.setNavigationItemSelectedListener(this);
 
 
