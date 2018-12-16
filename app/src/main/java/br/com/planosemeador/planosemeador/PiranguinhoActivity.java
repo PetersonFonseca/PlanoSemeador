20089b7548a4206d4faa549ba13266f9df598eec
Peterson Fonseca <petersonjf@gmail.com>
2018-12-16 13:01:51 -0200

    listas em todas as cidades

--- app/src/main/java/br/com/planosemeador/planosemeador/PiranguinhoActivity.java
+++ app/src/main/java/br/com/planosemeador/planosemeador/PiranguinhoActivity.java
@@ -2,8 +2,12 @@ package br.com.planosemeador.planosemeador;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,9 +17,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PiranguinhoActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Busca
+    DatabaseReference fornecedores;
+
+    //Lista
+    private RecyclerView recyclerView;
+    private List<Fornecedor> listaFornecedores = new ArrayList<>();
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +48,57 @@ public class PiranguinhoActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         setTitle("Semeador - Piranguinho");
+
+        //Lista inicio!
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //Configurar adapter
+        adapter = new Adapter( listaFornecedores );
+
+        //Configurar Recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration( new DividerItemDecoration(this, LinearLayout.VERTICAL));
+
+
+        // Inicio Busca
+
+        fornecedores = FirebaseDatabase.getInstance()
+                .getReference().child("fornecedores");
+
+        listaFornecedores.clear();
+
+        Query query2 = fornecedores.orderByChild("categoria").equalTo("piranguinho").limitToFirst(30);
+        query2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                        Fornecedor fornecedor = postSnapshot.getValue(Fornecedor.class);
+                        String nomeFornecedor = fornecedor.getNomeFornecedor();
+                        String enderecoFornecedor = fornecedor.getEnderecoFornecedor();
+                        String telefoneFornecedor = fornecedor.getTelefoneFornecedor();
+                        String desconto = fornecedor.getDesconto();
+                        Fornecedor fornecedorRecycleView = new Fornecedor( nomeFornecedor, "Tel: " +telefoneFornecedor, "Endereço: " + enderecoFornecedor, desconto + " %");
+
+                        listaFornecedores.add(fornecedorRecycleView);
+
+                    }
+                    recyclerView.setAdapter( adapter );
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        //Lista Fim
 
 
 
